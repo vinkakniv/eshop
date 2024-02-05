1fd47b7bc6a4bff78bafe9f5191d3b407f456893
vinkakniv <vinka.aeris@gmail.com>
2024-02-05 11:18:54 +0700

    implement edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("/product")
@@ -35,4 +36,22 @@ public class ProductController {
         model.addAttribute("products", allProducts);
         return "productList";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable String id, Model model) {
+        try {
+            Product product = service.findById(id);
+            model.addAttribute("product", product);
+            return "editProduct";
+        } catch (NoSuchElementException e) {
+            return "redirect:/product/list";
+        }
+    }
+
+    @PostMapping("/edit")
+    public String editProductPost(@ModelAttribute Product product) {
+        service.update(product);
+        return "redirect:/product/list";
+    }
+
 }
