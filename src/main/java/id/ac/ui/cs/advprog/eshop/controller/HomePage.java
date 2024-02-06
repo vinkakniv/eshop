e8c9cb0ff9d7e1ef7b7b43741932e94bada10504
vinkakniv <vinka.aeris@gmail.com>
2024-02-06 22:51:41 +0700

    implement homepage functional test

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePage.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePage {
+
+    @GetMapping("/")
+    public String createHomePage(Model model) {
+        return "homePage";
+    }
+
+}
