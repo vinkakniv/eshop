4c84a178816610d42b38a7dd249ce3ba19090b5f
vinkakniv <vinka.aeris@gmail.com>
2024-02-13 15:54:26 +0700

    fix issue: UnnecessaryModifier in product service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String id);
-    public Product update(Product product);
-    public void delete(Product product);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    Product update(Product product);
+    void delete(Product product);
 }
