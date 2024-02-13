39a1e36f79e091daee43dff9c52385460a43e2d3
vinkakniv <vinka.aeris@gmail.com>
2024-02-13 15:37:36 +0700

    fix issue: MethodNamingConventions

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EshopApplicationTest {
 
     @Test
-    void main_startsSpringApplication() {
+    void mainStartsSpringApplication() {
         EshopApplication.main(new String[] {});
     }
 }
