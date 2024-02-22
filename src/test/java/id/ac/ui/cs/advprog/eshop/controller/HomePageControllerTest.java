e2fc0b786c899fd18f2e4f6ca654481a0e5ffc17
vinkakniv <vinka.aeris@gmail.com>
2024-02-22 22:28:15 +0700

    fix homepage

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomePageController.class)
+public class HomePageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePageEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homePage"));
+    }
+}
