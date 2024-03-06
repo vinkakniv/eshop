21a9afe31114c7d2b5c228c0095eb835c95cf9f0
vinkakniv <vinka.aeris@gmail.com>
2024-03-06 17:42:42 +0700

    [RED] Make tests for Voucher Payment feature

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -87,8 +87,58 @@ class PaymentTest {
 
     @Test
     public void testValidPaymentMethod() {
-        Payment validPayment = new Payment("1", "VOUCHER_CODE", paymentData, order);
+        Payment validPayment = new Payment("9", "VOUCHER_CODE", paymentData, order);
         assertEquals(PaymentMethod.VOUCHER_CODE, validPayment.getMethod());
+    }
+
+    @Test
+    public void testValidVoucherPayment() {
+        Map<String, String> paymentData2 = new HashMap<>();
+        paymentData2.put("voucherCode", "ESHOP12345678ABC");
+        VoucherPayment voucherPay = new VoucherPayment("190", paymentData2, order);
+
+        assertEquals("SUCCESS", voucherPay.getStatus());
+        assertEquals("SUCCESS", order.getStatus());
+    }
+
+    @Test
+    public void testInvalidVoucherPayment() {
+        Map<String, String> paymentData3 = new HashMap<>();
+        paymentData3.put("voucherCode", "INVALID");
+        VoucherPayment voucherPay = new VoucherPayment("18888", paymentData3, order);
+
+        assertEquals("REJECTED", voucherPay.getStatus());
+        assertEquals("FAILED", order.getStatus());
+    }
+
+    @Test
+    public void testInvalidVoucherPaymentLength() {
+        Map<String, String> paymentData4 = new HashMap<>();
+        paymentData4.put("voucherCode", "ESHOP12345678AB"); // 15 characters
+        VoucherPayment payment = new VoucherPayment("17", paymentData4, order);
+
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals("FAILED", order.getStatus());
+    }
+
+    @Test
+    public void testInvalidVoucherPaymentPrefix() {
+        Map<String, String> paymentData5 = new HashMap<>();
+        paymentData5.put("voucherCode", "SHOP12345678ABCD"); // Does not start with "ESHOP"
+        VoucherPayment payment = new VoucherPayment("4", paymentData5, order);
+
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals("FAILED", order.getStatus());
+    }
+
+    @Test
+    public void testInvalidVoucherPaymentNum() {
+        Map<String, String> paymentData6 = new HashMap<>();
+        paymentData6.put("voucherCode", "ESHOP1234567ABCD"); // 7 numerical characters
+        VoucherPayment payment = new VoucherPayment("27", paymentData6, order);
+
+        assertEquals("REJECTED", payment.getStatus());
+        assertEquals("FAILED", order.getStatus());
     }
 
 }
